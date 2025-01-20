497fd3ecb955e592d38ecb73b88740e67e8e6af8
John Doe <johndoe@example.com>
2025-01-20 17:00:38 +0100

    verifyToken works

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+
+// UPDATE PUT @localhost/api/users/:id
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET ALL Users GET @localhost/api/users
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    // res.status(500).json(err);
+    next(err);
+  }
+};
+
+// GET One User GET @localhost/api/users/:i
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// DELETE @localhost/api/users/:id
+export const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted");
+    } else {
+      return next(createError(404, "no user found"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
 import express from "express";
 
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/userController.js";
+import { verifyToken } from "../utils/verifyToken.js";
+
 const router = express.Router();
 
-// @localhost/api/auth
-router.get("/", (req, res) => {
-  res.send("Hello, auth route");
+// CHeck Token GET @@localhost/api/users/checkauthentication
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
 });
 
-// @localhost/api/auth/register
-router.get("//register", (req, res) => {
-  res.send("register route");
-});
+// UPDATE PUT @localhost/api/users/:id
+router.put("/:id", updateUser);
+
+// DELETE @localhost/api/users/:id
+router.delete("/:id", deleteUser);
+
+// GET One User GET @localhost/api/users/:id
+router.get("/:id", getUser);
+
+// GET ALL Users GET @localhost/api/users/
+router.get("/", getUsers);
 
 export default router;
